ef923a7b2f768e391d860f0b464e082675968bf5
nngocjade <nngocjade@gmail.com>
2020-12-20 15:20:29 +0700

    changed getRandomColor function

--- anotherScript.js
+++ anotherScript.js
@@ -1,7 +1,7 @@
 let canvas = document.getElementById("myCanvas");
 let ctx = canvas.getContext("2d");
 
-// --------------------VARIABLES---------------------
+// ~~~~~~~~~~~~~~~~~~~~~~~[ VARIABLES ]~~~~~~~~~~~~~~~~~~~~~~~~~
 
 var x = canvas.width / 2;
 var y = canvas.height - 30;
@@ -14,46 +14,37 @@ var ballRadius = 10;
 var hex;
 var color = getRandomColor();
 
-var paddleHeight = 10;
+var paddleHeight = 20;
 var paddleWidth = 75;
 var paddleX = (canvas.width - paddleWidth) / 2;
 
 var rightPressed = false; //default value is False because at the beginning the control buttons are not pressed
 var leftPressed = false; //default value is False because at the beginning the control buttons are not pressed
 
-//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-// ~~~~~~~~~~~~~~~~~~~~~~~~ [ DRAW ] ~~~~~~~~~~~~~~~~~~~~~~~~~~
-function draw() {
-  // x & y coordintates of top left corner, the x & y coordinates of the bottom right corner
-  ctx.clearRect(0, 0, canvas.width, canvas.height); //This clears the canvas, each time, before the canvas draws (This allows the ball to move without a trail)
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//~~~~~~~~~~~~~~~~~~~[ BUTTONS TO CONTROL PADDLE ]~~~~~~~~~~~~~~~~
 
-  // CALLING DRAW BALL FUNCTION HERE
-  drawBall();
+//When the keydown event is fired on any of the keys on your keyboard (when they are pressed), the keyDownHandler() function will be executed. The same pattern is true for the second listener:
+document.addEventListener("keydown", keyDownHandler, false);
+document.addEventListener("keyup", keyUpHandler, false);
 
-  if (x + dx > canvas.width - ballRadius || x + dx < ballRadius) {
-    color = getRandomColor();
-    ctx.fillStyle = color;
-    dx = -dx;
-
-    /* console.group();
-     console.log("%cx-color", "color: green;");
-     console.log(color);
-     console.groupEnd(); */
+// When we press a key down, this information is stored in a variable.
+function keyDownHandler(e) {
+  if (e.key == "Right" || e.key == "ArrowRight") {
+    //The relevant variable in each case is set to true
+    //When the key is released, the variable is set back to false.
+    rightPressed = true;
+  } else if (e.key == "Left" || e.key == "ArrowLeft") {
+    leftPressed = true;
   }
+}
 
-  if (y + dy > canvas.height - ballRadius || y + dy < ballRadius) {
-    color = getRandomColor();
-    ctx.fillStyle = color;
-    dy = -dy;
-
-    /* console.group();
-    // console.log("%cy-color", "color:red;");
-    // console.log(color);
-       console.groupEnd(); */
+function keyUpHandler(e) {
+  if (e.key == "Right" || e.key == "ArrowRight") {
+    rightPressed = false;
+  } else if (e.key == "Left" || e.key == "ArrowLeft") {
+    leftPressed = false;
   }
-
-  x += dx;
-  y += dy;
 }
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 //~~~~~~~~~~~~~~~~~~~~~~~~[ DRAW BALL ]~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -75,25 +66,64 @@ function drawPaddle() {
   ctx.fill();
   ctx.closePath();
 }
-
-//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-// ~~~~~~~~~~~~~~~~~~~~[ COVERT TO DIFFERENT COLORS ]~~~~~~~~~~~~~~~~~~
-function convertToColor(num) {
-  return "#" + ("00000" + (num | 0).toString(16)).substr(-6);
-}
-//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-// ~~~~~~~~~~~~~~~~~~~~~[ GET A RANDOM HEX COLOR ]~~~~~~~~~~~~~~~~~~~~~
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// ~~~~~~~~~~~~~~~~~~~~~[ GET A RANDOM HEX COLOR ]~~~~~~~~~~~~~~~~
 function getRandomColor() {
-  color = "#" + (((1 << 24) * Math.random()) | 0).toString(16);
+  var letters = "0123456789ABCDEF".split("");
+  var color = "#";
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
   return color;
 }
-//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-//~~~~~~~~~~~~~~~~~~~[ ANIMATION CALL FUNCTION ]~~~~~~~~~~~~~~~~~~~~~~~
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// ~~~~~~~~~~~~~~~~~~~~~~~~ [ DRAW ] ~~~~~~~~~~~~~~~~~~~~~~~~~~
+function draw() {
+  // x & y coordintates of top left corner, the x & y coordinates of the bottom right corner
+  ctx.clearRect(0, 0, canvas.width, canvas.height); //This clears the canvas, each time, before the canvas draws (This allows the ball to move without a trail)
+
+  // CALLING DRAW BALL FUNCTION HERE
+  drawBall();
+  //CALLING DRAW PADDLE FUNCTION HERE
+  drawPaddle();
+
+  if (x + dx > canvas.width - ballRadius || x + dx < ballRadius) {
+    color = getRandomColor();
+    ctx.fillStyle = color;
+    dx = -dx;
+    /* console.group();
+     console.log("%cx-color", "color: green;");
+     console.log(color);
+     console.groupEnd(); */
+  }
+
+  if (y + dy > canvas.height - ballRadius || y + dy < ballRadius) {
+    color = getRandomColor();
+    ctx.fillStyle = color;
+    dy = -dy;
+    /* console.group();
+    // console.log("%cy-color", "color:red;");
+    // console.log(color);
+       console.groupEnd(); */
+  }
+
+  x += dx;
+  y += dy;
+
+  // THIS LOGIC MOVES THE PADDLE ON THE SCREEN
+  if (rightPressed) {
+    paddleX += 7; // If the right cursor is pressed, the paddle will move 7px to the right
+    if (paddleX + paddleWidth > canvas.width) {
+      paddleX = canvas.width - paddleWidth; //canvas.width-paddleWidth will prevent paddle from moving beyond the right side of the canvas width
+    }
+  } else if (leftPressed) {
+    paddleX -= 7; // If the left cursor is pressed, the paddle will move 7px to the left
+    if (paddleX < 0) {
+      // This prevents the paddle from moving beyond the left side of the canvas width
+      paddleX = 0;
+    }
+  }
+}
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//~~~~~~~~~~~~~~~~~~~[ ANIMATION CALL FUNCTION ]~~~~~~~~~~~~~~~~~~
 setInterval(draw, 10);
-
-//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-//~~~~~~~~~~~~~~~~~~~[ BUTTONS TO CONTROL PADDLE ]~~~~~~~~~~~~~~~~~~~~~
-
-//When the keydown event is fired on any of the keys on your keyboard (when they are pressed), the keyDownHandler() function will be executed. The same pattern is true for the second listener:
-document.addEventListener("keydown", keyDownHandler, false);
-document.addEventListener("keyup", keyUpHandler, false);
